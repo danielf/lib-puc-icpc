2412951101ceb272930a2daca1fcf041d8ccc6f1
Chaoxu Tong <blackchaoxu@gmail.com>
2013-03-07 21:38:00 -0500

    tip of graph_dev
    changed structure of graph.cpp
    new arc function to add vertices on the fly
    commented out other graph algorithms for future development
    emptied test.cpp

--- graph/test.cpp
+++ graph/test.cpp
@@ -1,11 +1,5 @@
-#include "../modelo.cpp"
+#include "../header.cpp"
 #include "graph.cpp"
 
 int main() {
-  graph g;
-  g.init(2);
-  g.arc(0, 1, 2, 3.0);
-  g.imb[0] = -1;
-  g.imb[1] = 1;
-  cout << g.mincostflow() << endl;
 }
